0d621ea692ee72e46bebcb2b79107d9e3f3c57c7
Chunlin Qin <hielvis@live.com>
2024-01-04 15:50:35 +0800

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://thegibook.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thegibook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
